a0b745e52784f5ff144232db1daaa14b0d5c2ac2
thaipt-0042 <pham.tri.thai@sun-asterisk.com>
2024-11-08 01:54:41 +0700

    add encrypt

--- index.js
+++ index.js
@@ -3,21 +3,79 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const https = require('https');
 
-app.get('/binance_all_symboys', (req, res) => {
+const steps = process.env.steps || 123;
+
+
+
+app.get('/all', (req, res) => {
   https.get('https://api1.binance.com/api/v3/ticker/price', (resp) => {
     let data = '';
 
-    // A chunk of data has been received.
     resp.on('data', (chunk) => {
       data += chunk;
     });
 
-    // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      res.json(JSON.parse(data));
+      console.log(req.query.symbols);
+
+      if (req.query.symbols) {
+        data = JSON.stringify(filterBySymbols(JSON.parse(data), req.query.symbols));
+      }
+
+      res.json(encrypt(data));
     });
   })
-  // https://devcenter.heroku.com/articles/getting-started-with-nodejs#deploy-the-app
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+
+function encrypt(text) {
+  let encrypted = '';
+
+  [...text].forEach(c => {
+    encrypted += (c.charCodeAt() + steps).toString().padStart(3, '0');
+  });
+
+  return encrypted;
+}
+
+
+function filterBySymbols(data, symbols) {
+  let output = [];
+  symbols = symbols.toLowerCase();
+
+  data.forEach(c => {
+    console.log(symbols, c.symbol.toLowerCase());
+    if (symbols.indexOf(c.symbol.toLowerCase()) !== -1) {
+      output.push(c);
+    }
+  });
+
+  return output;
+}
+
+
+// for extension
+function decrypt(encrypted) {
+  let output = '';
+
+  encrypted.match(/.{1,3}/g).forEach(asciiCode => {
+    output += String.fromCharCode(asciiCode - steps);
+  })
+
+  return output;
+}
+
+
+
+
+let houbiPrices = () => {
+  let endpoint = 'https://api.huobi.pro/market/tickers';
+}
+
+let binancePrices = () => {
+  let endpoint = 'https://api1.binance.com/api/v3/ticker/price';
+}
